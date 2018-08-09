6ebc083a431ee27d2b3578427b43da93abfac48d
Brent Klostermann <bklostermann@konradgroup.com>
2018-08-09 15:27:03 -0400

    course

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var db;
+if(process.env.ENV == 'Test') {
+    db = mongoose.connect('mongodb://localhost/bookAPI_test');
+} else {
+    db = mongoose.connect('mongodb://localhost/bookAPI');
+}
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var bookRouter = require('./routes/bookRoutes')(Book);
+app.use('/api/books', bookRouter);
+
+app.get('/', function (req, res) {
+    res.send('Welcome');
+});
+
+app.listen(port, function () {
+    console.log('Server running on PORT: ' + port);
+});
+
+module.exports = app;
